d7b2ab76f18a04118461abe18cb9b05a2a950366
jason-nordheim <jason.nordheim@gmail.com>
2020-05-27 10:23:34 -0600

    refactored functions for dogs controllers to dogs.js from app.js

--- app.js
+++ app.js
@@ -10,38 +10,6 @@ const indexController = (request, response) => {
     response.json({ message: "Hi!" })
 }
 
-const dogs [
-    {id: 1, name: "bixby"}, 
-    {id: 2, name: "alex"}
-]; 
-const listDogsController = (request, response) => {
-    const dog = dogs.find(d => d.id == request.params.id); 
-    response.json({dog})
-}
-const findNextDogId(){
-    let nextID = 1; 
-    dogs.forEach(dog => {
-        nextID = dog.id > nextID 
-            ? dog.id
-            : nextID 
-    });
-}
-const createDogController = (request, response) => {
-    const dog = {
-        id: findNextDogId(), 
-        name: request.body.name, 
-    }
-    dogs.push(dog); 
-    response.status(200).json({dog}); 
-}
-const getDogController = (request, response) => {
-    const dog = dogs.find(dog => dog.id == request.params.id); 
-    response.json({dog}); 
-}
-const deleteDogController = (request, response) => {
-    dogs = dogs.filter(dog => dog.id != request.params.id); 
-}
-
 /* Using routers - 
     since we created an express router for "dogs", we can say that 
     anything destined for "/dogs*" should be handled by the dogs router 
--- controllers/dogs.js
+++ controllers/dogs.js
@@ -1,6 +1,37 @@
 const express = require('express'); 
 const router = express.Router(); 
 
+const dogs [
+    {id: 1, name: "bixby"}, 
+    {id: 2, name: "alex"}
+]; 
+const listDogsController = (request, response) => {
+    const dog = dogs.find(d => d.id == request.params.id); 
+    response.json({dog})
+}
+const findNextDogId(){
+    let nextID = 1; 
+    dogs.forEach(dog => {
+        nextID = dog.id > nextID 
+            ? dog.id
+            : nextID 
+    });
+}
+const createDogController = (request, response) => {
+    const dog = {
+        id: findNextDogId(), 
+        name: request.body.name, 
+    }
+    dogs.push(dog); 
+    response.status(200).json({dog}); 
+}
+const getDogController = (request, response) => {
+    const dog = dogs.find(dog => dog.id == request.params.id); 
+    response.json({dog}); 
+}
+const deleteDogController = (request, response) => {
+    dogs = dogs.filter(dog => dog.id != request.params.id); 
+}
 
 router.get("/", listDogsController)
 router.get("/:id", getDogController)
